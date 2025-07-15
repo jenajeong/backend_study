0ee5e9481dd7dc95f69c5ba9c77ff9fb96a002a9
jenajeong <skg4660@gmail.com>
2025-07-15 22:38:03 +0900

    [BE]FIX: contig의 urls.py에 의미에 맞게 단어 수정

--- config/config/urls.py
+++ config/config/urls.py
@@ -21,7 +21,7 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('api/posts/', include('posts.urls')),
-    path('api/urls/', include('users.urls')),
+    path('api/users/', include('users.urls')),
     path('schema/', SpectacularAPIView.as_view(), name='schema'),
     path('docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
 ]
