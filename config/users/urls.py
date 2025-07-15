d89105594425fcf1e385f25d3bd0330ac2c3a995
jenajeong <skg4660@gmail.com>
2025-07-15 22:35:05 +0900

    [BE]FEATURE: users의 urls.py 수정(회원가입 추가)

--- config/users/urls.py
+++ config/users/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from .views import UserSignupView
 
 urlpatterns = [
-    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'), # 로그인 시 사용하는 API: (접근토큰과 리프레쉬)토큰 발급
+    path('refresh/', TokenRefreshView.as_view(), name='token_refresh'), # 토근 새로 발급해주는 API : 리프레쉬 토큰을 바탕으로 접근 토큰 새로 발급해 줌
+    path('signup/', UserSignupView.as_views(), name='user-signup'),
 ]
