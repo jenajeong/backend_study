f886413157c6501b954ab62e8c6c3d49ac7d3445
jenajeong <skg4660@gmail.com>
2025-07-15 22:28:45 +0900

    [BE]FEATURE: users의 view 구현(회원가입민 펄미션 설정)

--- config/users/views.py
+++ config/users/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from rest_framework import generics
+from .serializers import UserSignupSerializer
+from django.contrib.auth.models import User
+from rest_framework.permissions import AllowAny
 # Create your views here.
+
+class UserSignupView(generics.createAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSignupSerializer
+    permission_classes = [AllowAny]
