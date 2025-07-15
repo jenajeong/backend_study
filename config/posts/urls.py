a438230ad1f45cc36d7825cac6f62cb9b4d184e5
jenajeong <skg4660@gmail.com>
2025-07-15 21:58:08 +0900

    [BE]FEATURE: posts의 urls.py 설정

--- config/posts/urls.py
+++ config/posts/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
+from .views import PostList, PostCreateView, PostRetrieveUpdateDestroyView
 
 urlpatterns = [
-    
+    path('',PostList.as_view(), name='post-list'),
+    path('create/', PostCreateView.as_view(), name='post-create'),
+    path('<int:pk>',PostRetrieveUpdateDestroyView.as_view(), name='post-detail'),
 ]
