9d4d173aff26d4f3d3dd1b3fb6aae01baeef6ac8
jenajeong <skg4660@gmail.com>
2025-07-16 21:09:08 +0900

    [BE]FIX: posts의 permissions.py에서 basepermision에만 상속하도록 수정

--- config/posts/permissions.py
+++ config/posts/permissions.py
@@ -1,9 +1,8 @@
 from rest_framework import permissions
 from rest_framework.permissions import BasePermission
-
-class IsOwnerOrReadOnly(permissions,BasePermission): #작성자 이외엔 읽기만 ㄱ가능
+    
+class IsOwnerOrReadOnly(BasePermission):  # BasePermission만 상속, 작성자 이외엔 읽기만 가능
     def has_object_permission(self, request, view, obj):
-        if request.method in permissions.SAFE_METHODS: # GET, HAED, OPTIONS
-            
+        if request.method in permissions.SAFE_METHODS:  # GET, HEAD, OPTIONS 허용
             return True
-        return obj.author == request.user
+        return obj.author == request.user  # 이외는 작성자만 가능
