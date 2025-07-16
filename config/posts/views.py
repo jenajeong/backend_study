ad282eca55d010b18c644198c02080debacc2ce3
jenajeong <skg4660@gmail.com>
2025-07-16 20:48:59 +0900

    [BE]FEATRUE: posts의 views.py에서 좋아요 기능 추가(좋아요 추가 및 취소, 게시글 좋아요수 집계)

--- config/posts/views.py
+++ config/posts/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from django.contrib.auth.decorators import login_required
 from rest_framework import generics, permissions
-from .models import Post
+from .models import Post, Like
 from .permissions import IsOwnerOrReadOnly
 from .serializers import PostSerializer
+from django.http import JsonResponse
 # Create your views here.
 
 class PostListView(generics.ListAPIView):
@@ -22,6 +24,24 @@ class PostRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Post.objects.all()
     selializer_class = PostSerializer
     permission_classes = [IsOwnerOrReadOnly] #글 상세 조회, 수정, 삭제 - 로그인 시 가능
+
+@login_required #로그인 한 유저만
+def toggle_like(request, post_id):
+    if request.method == 'POST':
+        post = get_object_or_404(Post, id=post_id)
+        like, created = Like.objects.get_or_create(user=request.user, post=post) #Like모델에서 요청user와 post의 존재 여부에 따라 created 불린값 제공
+
+        if not created:
+            like.is_active = not like.is_active # 비활성화
+            like.save()
+        else:
+            like.is_active = True # 활성화
+            like.save()
+
+        is_liked = like.is_active
+        like_count = post.likes.filter(is_active=True).count() #해당 게시글의 좋아요수 집계
+
+        return JsonResponse({'liked': is_liked, 'like_count': like_count})
     
     
     
