0fa94874f397da8da0496229000ec3e1f26773f4
jenajeong <skg4660@gmail.com>
2025-07-16 21:12:12 +0900

    [BE]FIX: posts의 urls.py에서 views에서 잘못 import 된 클래스 수정

--- config/posts/urls.py
+++ config/posts/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import PostList, PostCreateView, PostRetrieveUpdateDestroyView,toggle_like
+from .views import PostListView, PostCreateView, PostRetrieveUpdateDestroyView,toggle_like
 
 urlpatterns = [
-    path('',PostList.as_view(), name='post-list'),
+    path('',PostListView.as_view(), name='post-list'),
     path('create/', PostCreateView.as_view(), name='post-create'),
     path('<int:pk>',PostRetrieveUpdateDestroyView.as_view(), name='post-detail'),
     path('post/<int:post_id>/like/', toggle_like, name='toggle_like'),
