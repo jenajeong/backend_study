c9f09e7416590afe213973c8a29070de3bdf7f3b
jenajeong <skg4660@gmail.com>
2025-07-16 21:17:52 +0900

    [BE]FIX: users의 urls.py의 오타 수정

--- config/users/urls.py
+++ config/users/urls.py
@@ -5,5 +5,5 @@ from .views import UserSignupView
 urlpatterns = [
     path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'), # 로그인 시 사용하는 API: (접근토큰과 리프레쉬)토큰 발급
     path('refresh/', TokenRefreshView.as_view(), name='token_refresh'), # 토근 새로 발급해주는 API : 리프레쉬 토큰을 바탕으로 접근 토큰 새로 발급해 줌
-    path('signup/', UserSignupView.as_views(), name='user-signup'),
+    path('signup/', UserSignupView.as_view(), name='user-signup'),
 ]
