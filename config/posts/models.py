6ca0fad16832fa9ba8c092212a3f45d67f0d80bf
jenajeong <skg4660@gmail.com>
2025-07-16 20:25:14 +0900

    [BE]FEATURE: post의 models.py에서 좋아요 모델 추가

--- config/posts/models.py
+++ config/posts/models.py
@@ -13,3 +13,11 @@ class Post(models.Model):
     
     def __str__(self):
         return self.title
+    
+class Like(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='likes')
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        unique_together = ('user', 'post')
