51a993c16c2508c89c951f2ae3d28334c1c8b265
jenajeong <skg4660@gmail.com>
2025-07-16 21:04:53 +0900

    [BE]FIX: posts의 permissions.py에 BasePermission import

--- config/posts/permissions.py
+++ config/posts/permissions.py
@@ -1,4 +1,5 @@
 from rest_framework import permissions
+from rest_framework.permissions import BasePermission
 
 class IsOwnerOrReadOnly(permissions,BasePermission): #작성자 이외엔 읽기만 ㄱ가능
     def has_object_permission(self, request, view, obj):
