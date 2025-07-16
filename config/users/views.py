a853c09461be6e735d8a9b066abd35511df15e86
jenajeong <skg4660@gmail.com>
2025-07-16 21:16:04 +0900

    [BE]FIX: users의 views.py에서 오타 수정

--- config/users/views.py
+++ config/users/views.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 from rest_framework.permissions import AllowAny
 # Create your views here.
 
-class UserSignupView(generics.createAPIView):
+class UserSignupView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSignupSerializer
     permission_classes = [AllowAny]
