976c380a6900c3166dfd45951680e27ee916a61a
jenajeong <skg4660@gmail.com>
2025-07-23 22:10:53 +0900

    [BE]FEATURE: 댓글과 대댓글 url 설정 (posts/urls.py)

--- config/posts/urls.py
+++ config/posts/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import PostListView, PostCreateView, PostRetrieveUpdateDestroyView,toggle_like
+from .views import PostListView, PostCreateView, PostRetrieveUpdateDestroyView, toggle_like, CommentListCreateView, CommentUpdateDeleteView, ReplyCreateView
 
 urlpatterns = [
     path('',PostListView.as_view(), name='post-list'),
     path('create/', PostCreateView.as_view(), name='post-create'),
     path('<int:pk>',PostRetrieveUpdateDestroyView.as_view(), name='post-detail'),
     path('post/<int:post_id>/like/', toggle_like, name='toggle_like'),
+    path('community/<int:community_id>/comments/', CommentListCreateView.as_view()),
+    path('community/<int:community_id>/comments/<int:pk>/', CommentUpdateDeleteView.as_view()),
+    path('community/comments/<int:comment_id>/replies/', ReplyCreateView.as_view()),
 ]
