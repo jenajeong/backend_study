a0b2233834787ca945d20f5ea4ecaa08eb2932d8
jenajeong <skg4660@gmail.com>
2025-07-23 22:06:45 +0900

    [BE]FEATURE: 댓글 시리얼라이저 생성 (posts/serializers.py)

--- config/posts/serializers.py
+++ config/posts/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Post
+from .models import Post, Comment
 
 class PostSerializer(serializers.ModelSerializer):
     author_username = serializers.CharField(source='author.username', read_only=True)
@@ -8,3 +8,19 @@ class PostSerializer(serializers.ModelSerializer):
         model = Post
         fields = ['id','title','content','author','author_username','created_at','updated_at']
         read_only_fields = ['author','created_at','updated_at']
+        
+class ReplySerializer(serializers.ModelSerializer):
+    author = serializers.StringRelatedField(read_only=True)
+
+    class Meta:
+        model = Comment
+        fields = ['id', 'content', 'author', 'created', 'parent']
+
+class CommentSerializer(serializers.ModelSerializer):
+    author = serializers.StringRelatedField(read_only=True)
+    replies = ReplySerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Comment
+        fields = ['id', 'content', 'author', 'created', 'parent', 'replies']
+        
