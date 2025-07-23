4f51295d57a96f900179e444b43294f42c827438
jenajeong <skg4660@gmail.com>
2025-07-23 22:04:33 +0900

    [BE]FEATURE: 댓글 모델 생성(posts/models.py)

--- config/posts/models.py
+++ config/posts/models.py
@@ -2,7 +2,9 @@ from django.db import models
 
 # Create your models here.
 
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
@@ -23,3 +25,15 @@ class Like(models.Model):
     
     class Meta:
         unique_together = ('user', 'post')
+
+class Comment(models.Model):
+    post = models.ForeignKey('Post', on_delete=models.CASCADE, related_name='comments')  # 게시글
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    parent = models.ForeignKey('self', null=True, blank=True, on_delete=models.CASCADE, related_name='replies')  # 대댓글이면 부모 지정
+    is_deleted = models.BooleanField(default=False)  # 소프트 삭제
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.content[:30]
+    
